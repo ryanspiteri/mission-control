1c94b6fa29322999e232b0c28dd371c6e0115193
Elara Voss <elara@onesthealth.com>
2026-04-02 21:07:57 +0000

    fix: property finder with addresses — 15 deals, 110 listings scanned

--- prop.js
+++ prop.js
@@ -1,48 +1,138 @@
-// Property Finder — auto-generated 2026-04-02 21:02
+// Property Finder — auto-generated 2026-04-02 21:07
 var PROP_DEALS = [
   {
+    "url": "https://www.domain.com.au/24-62-66-brown-street-labrador-qld-4215-2020628543",
+    "address": "24/62-66 Brown Street, Labrador QLD 4215",
+    "price": "Offers Over $900,000",
+    "beds": 3,
+    "baths": 1,
+    "days": 0,
+    "type": "Townhouse"
+  },
+  {
     "url": "https://www.domain.com.au/9-ragamuffin-drive-west-coomera-waters-qld-4209-2020733053",
-    "address": "",
+    "address": "9 Ragamuffin Drive West, Coomera Waters QLD 4209",
     "price": "Offers Over $1,550,000",
-    "days": 0
+    "beds": 4,
+    "baths": 2,
+    "days": 0,
+    "type": "House"
   },
   {
     "url": "https://www.domain.com.au/208-641-pine-ridge-road-biggera-waters-qld-4216-2020726377",
-    "address": "",
+    "address": "208/641 Pine Ridge Road, Biggera Waters QLD 4216",
     "price": "Offers Over $790,000",
-    "days": 0
+    "beds": 3,
+    "baths": 1,
+    "days": 0,
+    "type": "Townhouse"
   },
   {
     "url": "https://www.domain.com.au/25-43-myola-court-coombabah-qld-4216-2020726463",
-    "address": "",
+    "address": "25/43 Myola Court, Coombabah QLD 4216",
     "price": "1,050,000+",
-    "days": 0
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "Townhouse"
   },
   {
     "url": "https://www.domain.com.au/10-mackenzie-street-coomera-qld-4209-2020727657",
-    "address": "",
+    "address": "10 Mackenzie Street, Coomera QLD 4209",
     "price": "Vacant & Move In Ready | Offers Over $1,099,000+",
-    "days": 0
+    "beds": 4,
+    "baths": 2,
+    "days": 0,
+    "type": "House"
   },
   {
     "url": "https://www.domain.com.au/21-binalong-drive-ashmore-qld-4214-2020728332",
-    "address": "",
+    "address": "21 Binalong Drive, Ashmore QLD 4214",
     "price": "Offers Over $1,395,000",
-    "days": 0
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "House"
   },
   {
     "url": "https://www.domain.com.au/30-studio-drive-oxenford-qld-4210-2020721586",
-    "address": "",
+    "address": "30 Studio Drive, Oxenford QLD 4210",
     "price": "Pool & Dual Street Access! Offers Over $1,179,000",
-    "days": 0
+    "beds": 4,
+    "baths": 1,
+    "days": 0,
+    "type": "House"
   },
   {
     "url": "https://www.domain.com.au/57-flora-terrace-pimpama-qld-4209-2020723569",
-    "address": "",
+    "address": "57 Flora Terrace, Pimpama QLD 4209",
     "price": "Offers Over $950,000",
-    "days": 0
+    "beds": 4,
+    "baths": 2,
+    "days": 0,
+    "type": "House"
+  },
+  {
+    "url": "https://www.domain.com.au/4-14-victor-avenue-paradise-point-qld-4216-2020462373",
+    "address": "4/14 Victor Avenue, Paradise Point QLD 4216",
+    "price": "$1,390,000 LAST REMAINING",
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "Villa"
+  },
+  {
+    "url": "https://www.domain.com.au/73-girtin-circuit-pimpama-qld-4209-2020648188",
+    "address": "73 Girtin Circuit, Pimpama QLD 4209",
+    "price": "Offers Over $1,250,000",
+    "beds": 4,
+    "baths": 2,
+    "days": 0,
+    "type": "House"
+  },
+  {
+    "url": "https://www.domain.com.au/145-125-santa-cruz-boulevard-clear-island-waters-qld-4226-2020682537",
+    "address": "145/125 Santa Cruz Boulevard, Clear Island Waters QLD 4226",
+    "price": "$1,475,000 plus",
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "Townhouse"
+  },
+  {
+    "url": "https://www.domain.com.au/57-1-residences-circuit-pimpama-qld-4209-2020716301",
+    "address": "57/1 Residences Circuit, Pimpama QLD 4209",
+    "price": "Asking Over $900,000",
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "House"
+  },
+  {
+    "url": "https://www.domain.com.au/tugun-1-pendraat-parade-hope-island-qld-4212-2018399356",
+    "address": "Tugun/1 Pendraat Parade, Hope Island QLD 4212",
+    "price": "$1,090,000",
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "Unknown"
+  },
+  {
+    "url": "https://www.domain.com.au/pelican-1-pendraat-parade-hope-island-qld-4212-2019328017",
+    "address": "Pelican//1 Pendraat Parade, Hope Island QLD 4212",
+    "price": "$975,000",
+    "beds": 3,
+    "baths": 2,
+    "days": 0,
+    "type": "Unknown"
+  },
+  {
+    "url": "https://www.domain.com.au/2-12-goldwyn-way-oxenford-qld-4210-2020709483",
+    "address": "2/12 Goldwyn Way, Oxenford QLD 4210",
+    "price": "Offers Over $875,000",
+    "beds": 3,
+    "baths": 1,
+    "days": 0,
+    "type": "House"
   }
 ];
-var PROP_EMAIL_IDS = [];
-var PROP_DRAFTS = {};
-var propSortCol = null;
